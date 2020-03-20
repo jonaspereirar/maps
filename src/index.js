f9c0261c17047adc62dc72f39b4b153482104473
jonaspereirar <jonasprodrigues@gmail.com>
2020-03-20 01:04:43 +0000

    Fix commit: Teste Api Maps-Geolocation

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+
+import MapView from 'react-native-maps'
+
+import Geolocation from "@react-native-community/geolocation";
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFill,
+    backgroundColor: "#7159c1",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coordinates, setCoordinates] = useState({});
+  //const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      ({ coords }) => {
+        setCoordinates(coords);
+        setLoading(false);
+      },
+      error => {
+        console.log(error);
+      },
+      { enableHighAccuracy: true, maximumAge: 10000, timeout: 10000 }
+    );
+  }, []);
+
+  
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <MapView
+          initialRegion={{
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude,
+            latitudeDelta: 0.0068,
+            longitudeDelta: 0.0068
+          }}
+          style={styles.map}
+        >
+        </MapView>
+      )}
+    </View>
+  )
+}
+
+export default App;
